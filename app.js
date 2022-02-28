e77128241751596beaa7f66cefd1e63975522131
e1730934 <e1730934@cmaisonneuve.qc.ca>
2022-02-27 19:34:14 -0500

    Eslint

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 // const path = require('path');
 const cors = require('cors');
-const requeteKnex = require("./database/requeteKnex");
+const requeteKnex = require('./database/requeteKnex');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,175 +11,168 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('admin'));
 // app.set("json spaces", 2)
 
-app.post("/login", async (req, rep) => {
+app.post('/login', async (req, rep) => {
+  try {
+    let connection = false;
+    const userConnect = {
+      Identifiant: req.body.Identifiant,
+      MotDePasse: req.body.MotDePasse,
+    };
+    const donnees = await requeteKnex.getUtilisateurs();
 
-    try {
-        let connection = false;
-        const userConnect = {
-            Identifiant: req.body.Identifiant,
-            MotDePasse: req.body.MotDePasse,
-        }
-        const donnees = await requeteKnex.getUtilisateurs();
-
-        for (i = 0; i < donnees.length; i++) {
-            if ((userConnect.Identifiant === donnees[i].Identifiant) && (userConnect.MotDePasse === donnees[i].MotDePasse)) {
-                rep.status(200).json({
-                    success: true,
-                    nom_utilisateur: donnees[i].Identifiant
-                });
-                connection = true;
-                console.log(userConnect.Identifiant, "vien de se connecter");
-            }
-        }
-        if (!connection) {
-            rep.status(200).json({
-                success: false,
-            });
-        }
-    } catch (error) {
-
-        rep.status(500).json({
-            success: false,
-            erreur: error
+    for (let i = 0; i < donnees.length; i++) {
+      if ((userConnect.Identifiant === donnees[i].Identifiant)
+          && (userConnect.MotDePasse === donnees[i].MotDePasse)) {
+        rep.status(200).json({
+          success: true,
+          nom_utilisateur: donnees[i].Identifiant,
         });
+        connection = true;
+        console.log(userConnect.Identifiant, 'vien de se connecter');
+      }
     }
+    if (!connection) {
+      rep.status(200).json({
+        success: false,
+      });
+    }
+  } catch (error) {
+    rep.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
 });
 
-app.get("/rechercher/IPPE", async (req, res) => {
-    try {
-        let IdPersonnes = await requeteKnex.getIdPersonnes(req.params.NomFamille, req.params.Prenom1, req.params.Prenom2, req.params.Masculin, req.params.DateNaissance);
-        // let IdPersonnes = await requeteKnex.getIPPE("Ducharme", "Benoit", null, true, "1975-08-31");
-        console.log(IdPersonnes)
-        return res.status(200).json(IdPersonnes);
+app.get('/rechercher/IPPE', async (req, res) => {
+  try {
+    const IdPersonnes = await requeteKnex
+      .getIPPE(
+        req.params.NomFamille,
+        req.params.Prenom1,
+        req.params.Prenom2,
+        req.params.Masculin,
+        req.params.DateNaissance,
+      );
+    // let IdPersonnes = await requeteKnex.getIPPE("Ducharme", "Benoit", null, true, "1975-08-31");
+    console.log(IdPersonnes);
+    return res.status(200).json(IdPersonnes);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      erreur: error.message,
+    });
+  }
+});
 
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error.message
-        });
-    }
-})
+app.post('/rechercher/infos/personnes', async (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  try {
+    const { NomFamille } = req.body;
+    const { Prenom1 } = req.body;
+    let { Prenom2 } = req.body;
+    const Masculin = Boolean(req.body.Sexe);
+    const { DateNaissance } = req.body;
 
-app.post("/rechercher/infos/personnes", async (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    try {
-        let NomFamille = req.body.NomFamille
-        let Prenom1 = req.body.Prenom1
-        let Prenom2 = req.body.Prenom2
-        let Masculin =  Boolean(req.body.Sexe)
-        let DateNaissance =req.body.DateNaissance
+    if (Prenom2 === '') { Prenom2 = null; }
+    const InfosPersonnes = await requeteKnex
+      .getIPPE(NomFamille, Prenom1, Prenom2, Masculin, DateNaissance);
+    console.log(InfosPersonnes);
+    res.status(200).json({
+      success: true,
+      data: InfosPersonnes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
-        if(Prenom2 ===''){Prenom2=null}
-        let InfosPersonnes = await requeteKnex.getIPPE(NomFamille, Prenom1,Prenom2 ,Masculin, DateNaissance );
-        // let InfosPersonnes = await requeteKnex.getInfosPersonnes("Ducharme", "Benoit", null, true, "1975-08-31");
-        console.log(InfosPersonnes)
-        res.status(200).json({
-            "success": true,
-            "data": InfosPersonnes
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
+app.get('/conditions', async (req, res) => {
+  try {
+    const conditions = await requeteKnex.getConditions();
+    res.status(200).json(conditions);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
-app.get("/conditions", async (req, res) => {
-    try {
-        let conditions = await requeteKnex.getConditions();
-        res.status(200).json(conditions);
+app.get('/FPS', async (req, res) => {
+  try {
+    const FPS = await requeteKnex.getFPS();
+    res.status(200).json(FPS);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
+app.get('/IBAF', async (req, res) => {
+  try {
+    const IBAF = await requeteKnex.getIBAF();
+    res.status(200).json(IBAF);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
-app.get("/FPS", async (req, res) => {
-    try {
-        let FPS = await requeteKnex.getFPS();
-        res.status(200).json(FPS);
+app.get('/IBOB', async (req, res) => {
+  try {
+    const IBOB = await requeteKnex.getIBOB();
+    res.status(200).json(IBOB);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
+app.get('/IBVA', async (req, res) => {
+  try {
+    const IBVA = await requeteKnex.getIBVA();
+    res.status(200).json(IBVA);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
+app.get('/Personnes', async (req, res) => {
+  try {
+    const Personnes = await requeteKnex.getPersonnes();
+    res.status(200).json(Personnes);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
-app.get("/IBAF", async (req, res) => {
-    try {
-        let IBAF = await requeteKnex.getIBAF();
-        res.status(200).json(IBAF);
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
-
-
-app.get("/IBOB", async (req, res) => {
-    try {
-        let IBOB = await requeteKnex.getIBOB();
-        res.status(200).json(IBOB);
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
-
-
-app.get("/IBVA", async (req, res) => {
-    try {
-        let IBVA = await requeteKnex.getIBVA();
-        res.status(200).json(IBVA);
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
-
-
-
-app.get("/Personnes", async (req, res) => {
-    try {
-        let Personnes = await requeteKnex.getPersonnes();
-        res.status(200).json(Personnes);
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
-
-app.get("/Utilisateurs", async (req, res) => {
-    try {
-        let Utilisateurs = await requeteKnex.getUtilisateurs();
-        res.status(200).json(Utilisateurs);
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            erreur: error
-        });
-    }
-})
+app.get('/Utilisateurs', async (req, res) => {
+  try {
+    const Utilisateurs = await requeteKnex.getUtilisateurs();
+    res.status(200).json(Utilisateurs);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      erreur: error,
+    });
+  }
+});
 
 // app.put("/adduser/:id", async (req, res) => {
 //   try {
@@ -193,7 +187,6 @@ app.get("/Utilisateurs", async (req, res) => {
 //   }
 // })
 
-
 app.listen(PORT, () => {
-    console.log(`Mon application roule sur http://localhost:${PORT}`);
+  console.log(`Mon application roule sur http://localhost:${PORT}`);
 });
