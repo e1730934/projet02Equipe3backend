b21a93568f7d06cf7ad092a274aa34326800c29c
DavidDechaine <david.dechaine90@gmail.com>
2022-03-28 03:36:05 -0400

    Rectifier erreur et adaptation
    adapation de la version du professeur

--- app.js
+++ app.js
@@ -2,64 +2,62 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const request = require('./requetesKnex');
-const { default: knex } = require('knex');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/login', async (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
 
-	try {
-		const { identifiant, motDePasse } = req.body;
-		const resultat = await request.connexion(identifiant, motDePasse);
+    let resultat;
+    try {
+        const { identifiant, motDePasse } = req.body;
+        resultat = await request.connexion(identifiant, motDePasse);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
 
-		if(resultat.length!=0){
-			//envoi du message contenant les information pour le login
-			/**** TEMPORAIRE JUSQU'A TEMPS QUE L'ON VOIT LES NOTION DE TOKEN*****/
-			return res.status(200).json({
-				'succes' : true,
-				'Etudiant': resultat[0].Etudiant,
-				'Matricule': resultat[0].Identifiant,
-				'Nom': resultat[0].NomFamille });
-	
-		} else 
-			return res.status(404).json({'succes' : false});
-	} catch (error) {
-		res.status(500).json(error.message);
-	}
+    if (resultat.length === 0) {
+        // envoi du message contenant les information pour le login
+        /** ** TEMPORAIRE JUSQU'A TEMPS QUE L'ON VOIT LES NOTION DE TOKEN**** */
 
+        return res.status(404).json({ succes: false });
+    }
+
+    return res.status(200).json({
+        succes: true,
+        Etudiant: resultat[0].Etudiant,
+        Matricule: resultat[0].Identifiant,
+        Nom: resultat[0].NomFamille,
+    });
 });
 
 app.get('/ippeInfo', async (req, res) => {
+    let resultat;
 
-	try {
-		const { IdPersonne } = req.body;
-		const resultat = await request.personneAjoutOuModification(IdPersonne);
+    const { nomFamille, prenom1 } = req.query;
+    const prenom2 = (req.query.prenom2 === '') ? null : req.query.prenom2;
+    const masculin = (req.query.masculin === 'true');
+    const { dateNaissance } = req.query;
 
-		if(resultat.length!=0){
-			//envoi du message contenant les information pour le post de personne dans base de donnee
+    if (nomFamille === undefined || prenom1 === undefined || prenom2 === undefined
+        || masculin === undefined || dateNaissance === undefined) {
+        return res.status(400).json('paramètre manquant');
+    }
+    try {
+        resultat = await request.getIPPE(nomFamille, prenom1, prenom2, masculin, dateNaissance);
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
 
-			return res.status(200).json({
-				'succes' : true,
-				'Nom': resultat[0].NomFamille,
-				'Prenom1': resultat[0].Prenom1,
-				'Prenom2': resultat[0].Prenom2,
-				'Sexe': resultat[0].Masculin,
-				'DateNaissance': resultat[0].DateNaissance,
-				'Sexe': resultat[0].Masculin,
-				'Catégorie': resultat[0].TypePersonne			
-				
-			 });
-	
-		} else 
-			return res.status(404).json({'succes' : false});
-	} catch (error) {
-		res.status(500).json(error.message);
-	}
-   
+    if (resultat.length === 0) {
+        return res.status(404).json('Cette personne n\'est pas répertoriée');
+    }
+
+    return res.status(200).json(resultat);
 });
+
 
 app.get('/personnes', async (req, res) => {
     // Pour quand on uilisera les tokens
@@ -161,8 +159,7 @@ app.put('/personnes', async (req, res) => {
         );
         res.status(200).json('Personne modifiée :)');
     } catch (error) {
-        res.status(500).json(error.message);
-        res.status(404).send('La personne n\'existe pas!');
+        res.status(500).json('Valeurs transmises invalides (veuillez vérifier la date)');
     }
     /* {
         "TypePersonne": "Enseignant",
