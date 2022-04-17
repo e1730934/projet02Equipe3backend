a839b11c0f42491402c16ea6255a206c3f245724
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-04-17 14:14:00 -0400

    Fix Regex

--- routes/armes.js
+++ routes/armes.js
@@ -25,14 +25,18 @@ router.post('/', async (req, res) => {
     const {
         noSerie, marque, calibre, typeArme,
     } = req.body;
-    const regexJJ = /^(0[1-9]|1[0-2])$/;
-    const regexMM = /^\d{1,2}$/;
-    const regexAA = /^\d{2}$/;
-    const regexSChiffres = /^\d{4}$/;
-    const validationEvent = ((regexJJ.test(req.body.JJ) && regexMM.test(req.body.MM)
-        && regexAA.test(req.body.AA) && regexSChiffres.test(req.body.sequenceChiffres)));
     const noEvenement = `${req.body.NoCours}-${req.body.AA}${req.body.MM
     }${req.body.JJ}-${req.body.sequenceChiffres}`;
+    const regexJJ = /^0[1-9]|[12]\d|3[01]$/;
+    const regexMM = /^0[1-9]|1[0-2]$/;
+    const regexAA = /^\d{2}$/;
+    const regexSChiffres = /^\d{4}$/;
+    const validationEvent = (
+        regexJJ.test(req.body.JJ)
+        && regexMM.test(req.body.MM)
+        && regexAA.test(req.body.AA)
+        && regexSChiffres.test(req.body.sequenceChiffres)
+    );
     if (validationEvent === false) {
         return res.status(400).json({ message: 'Numéro d\'événement invalide' });
     }
@@ -56,7 +60,7 @@ router.post('/', async (req, res) => {
         } else {
             res.status(500).json({
                 success: false,
-                message: 'Une erreur est survenue, l\'action n\'a pas été effectuée',
+                message: 'Une erreur est survenue, Le numéro de série existe déjà dans la base de données.',
             });
         }
     } catch (error) {
@@ -73,12 +77,16 @@ router.put('/', async (req, res) => {
     } = req.body;
     const noEvenement = `${req.body.NoCours}-${req.body.AA}${req.body.MM
     }${req.body.JJ}-${req.body.sequenceChiffres}`;
-    const regexJJ = /^(0[1-9]|1[0-2])$/;
-    const regexMM = /^\d{1,2}$/;
+    const regexJJ = /^0[1-9]|[12]\d|3[01]$/;
+    const regexMM = /^0[1-9]|1[0-2]$/;
     const regexAA = /^\d{2}$/;
     const regexSChiffres = /^\d{4}$/;
-    const validationEvent = ((regexJJ.test(req.body.JJ) && regexMM.test(req.body.MM)
-        && regexAA.test(req.body.AA) && regexSChiffres.test(req.body.sequenceChiffres)));
+    const validationEvent = (
+        regexJJ.test(req.body.JJ)
+        && regexMM.test(req.body.MM)
+        && regexAA.test(req.body.AA)
+        && regexSChiffres.test(req.body.sequenceChiffres)
+    );
     if (validationEvent === false) {
         return res.status(400).json({ message: 'Numéro d\'événement invalide' });
     }
